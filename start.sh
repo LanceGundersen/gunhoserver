bb72c66e217129a0e3990c05430e469077d66607
Lance <LanceGundersen@users.noreply.github.com>
2020-01-18 13:37:00 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,12 +8,10 @@ echo "After a fresh install you must log out and back in again!"
 echo "====="
 echo "====="
 
-sudo chmod -x portainer.sh
-
 echo "Fresh Install?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) ./fresh_install.sh; break;;
+        Yes ) sudo chmod -x fresh_install.sh;  . fresh_install.sh; break;;
         No ) break;;
     esac
 done
@@ -21,7 +19,7 @@ done
 echo "Install Portainer?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo chmod -x portainer.sh; ./portainer.sh; break;;
+        Yes ) sudo chmod -x portainer.sh; . portainer.sh; break;;
         No ) break;;
     esac
 done
