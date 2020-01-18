589813e5bff0f3c575ca967fe737ccb582a1e1fd
Lance <LanceGundersen@users.noreply.github.com>
2020-01-18 14:01:02 -0500

    Update livepatch.sh

--- livepatch.sh
+++ livepatch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "Get a token here: https://auth.livepatch.canonical.com/"
-read key
+echo "Continue...?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) read -p "Enter Key: " key;;
+        No ) break;;
+    esac
+done
 
 sudo snap install canonical-livepatch
 sudo canonical-livepatch enable $key
