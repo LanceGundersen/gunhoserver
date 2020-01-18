2b70695d41d102e03265129319d7bbb23a25b9ea
Lance <LanceGundersen@users.noreply.github.com>
2020-01-18 13:27:29 -0500

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Install and Start Portainer..."
+docker volume create portainer_data
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+echo "Verify Portainer Running..."
+docker ps
