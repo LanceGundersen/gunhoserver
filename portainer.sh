1946f777203b00d7a6132e3069a1bc743a157306
Lance <LanceGundersen@users.noreply.github.com>
2020-01-18 19:12:45 -0500

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -2,7 +2,7 @@
 
 echo "Install and Start Portainer..."
 docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run --name=portainer --restart=unless-stopped -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 echo "Verify Portainer Running..."
 docker ps
